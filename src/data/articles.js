981d78ee0facf19931b27ffba0cbccc5129f2a7b
unknown <sarahemir98@gmail.com>
2025-09-01 18:53:06 +0500

    первая версия

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,10 +1,10 @@
 export const articles = [
   {
     id: 'oxygen',
-    title: "The effect of oxygen on mortality in the population",
-    summary: "Influence of oxygen",
-    pdf: "/articles/MADS-panda-sat-math.pdf",
-    image: "/images/ai-cutout.png",
+    title: "Getting to know the newspaper",
+    summary: "The first online newspaper of 165 lyceum",
+    pdf: "/articles/First_School_Newspaper_Lyceum165_with_photo.pdf",
+    image: "/images/firstphoto.png",
   },
   {
     id: 'oxygen2',
@@ -20,4 +20,5 @@ export const articles = [
     pdf: "/articles/MADS-panda-sat-math.pdf",
     image: "/images/ai-cutout.png",
   },
+  
 ];
