90e73dc4ceef68b69cb50b491bc459f1381a23e2
unknown <sarahemir98@gmail.com>
2025-09-02 11:26:18 +0500

    тест изменение один из последних вариантов

--- src/data/articles.js
+++ src/data/articles.js
@@ -3,7 +3,7 @@ export const articles = [
     id: 'oxygen',
     title: "Getting to know the newspaper",
     summary: "The first online newspaper of 165 lyceum",
-    pdf: "/articles/First_School_Newspaper_Lyceum165_with_photo.pdf",
+    pdf: "/articles/конвертация.pdf",
     image: "/images/firstphoto.png",
   },
   {
