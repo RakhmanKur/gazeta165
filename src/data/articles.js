ef7078a41855564b827e833564167f12690243b7
unknown <sarahemir98@gmail.com>
2025-09-03 18:24:44 +0500

    изменения

--- src/data/articles.js
+++ src/data/articles.js
@@ -6,19 +6,5 @@ export const articles = [
     pdf: "/articles/конвертация.pdf",
     image: "/images/firstphoto.png",
   },
-  {
-    id: 'oxygen2',
-    title: "The effect of oxygen on mortality in the population",
-    summary: "Influence of oxygen",
-    pdf: "/articles/MADS-panda-sat-math.pdf",
-    image: "/images/ai-cutout.png",
-  },
-  {
-    id: 'oxygen3',
-    title: "The effect of oxygen on mortality in the population",
-    summary: "Influence of oxygen",
-    pdf: "/articles/MADS-panda-sat-math.pdf",
-    image: "/images/ai-cutout.png",
-  },
-  
+
 ];
