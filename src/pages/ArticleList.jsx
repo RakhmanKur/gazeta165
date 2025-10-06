3ecad7c9338d4f0e793b715401f6f1010549ca92
unknown <sarahemir98@gmail.com>
2025-10-06 19:08:21 +0500

    nothing

--- src/pages/ArticleList.jsx
+++ src/pages/ArticleList.jsx
@@ -1,17 +1,32 @@
-import { Link } from 'react-router-dom'
-import { articles } from '../data/articles'
-import ArticleCard from '../components/ArticleCard'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { articles as STATIC_ARTICLES } from "../data/articles";
+import ArticleCard from "../components/ArticleCard";
 
-export default function ArticleList(){
+export default function ArticleList() {
+  const [allArticles, setAllArticles] = useState(STATIC_ARTICLES);
+
+  // Подгружаем статьи из localStorage (добавленные через админку)
+  useEffect(() => {
+    const savedArticles = JSON.parse(localStorage.getItem("articles")) || [];
+    // Добавляем их к статическим, чтобы все показывались вместе
+    setAllArticles([...STATIC_ARTICLES, ...savedArticles]);
+  }, []);
+
+  // Если нет статей, ничего не рендерим
+  if (allArticles.length === 0) {
+    return <p>Пока нет статей 😅</p>;
+  }
+
   return (
     <div className="grid gap-6">
       <div className="md:col-span-2">
-        <Link to={`/article/${articles[0].id}`}>
-          <ArticleCard article={articles[0]} />
+        <Link to={`/article/${allArticles[0].id}`}>
+          <ArticleCard article={allArticles[0]} />
         </Link>
 
         <div className="mt-6 grid gap-4 md:grid-cols-2">
-          {articles.slice(1).map(a=>(
+          {allArticles.slice(1).map((a) => (
             <Link key={a.id} to={`/article/${a.id}`}>
               <ArticleCard article={a} />
             </Link>
@@ -19,5 +34,5 @@ export default function ArticleList(){
         </div>
       </div>
     </div>
-  )
+  );
 }
